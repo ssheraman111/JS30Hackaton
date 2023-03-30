4ede92476d2f9dc2688e400c1aeb7de602e3e85a
Шераман Суеркулов <ssheraman@Seramans-MacBook-Pro.local>
2023-03-30 11:18:49 +0600

    merging

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import "./Footer.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div>
+      <div className="footerContainer">
+        <div className="HeadFooter">
+          <div>GardenCare</div>
+          <div>Plant Renovation</div>
+          <div>Watering Garden</div>
+          <div>Newsletters</div>
+        </div>
+        <div className="BodyFooter">
+          <div>Logo</div>
+          <div>Adress</div>
+          <div>Contacts</div>
+          <div>Phone</div>
+        </div>
+        <div className="FootFooter">
+          <ul>My Accout</ul>
+          <ul>Help & Guide</ul>
+          <ul>Categories</ul>
+          <ul>Social Media</ul>
+        </div>
+        <div className="FooterBottom">
+          © 2021 GreenShop. All Rights Reserved.
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
--- src/routes/MainRoutes.js
+++ src/routes/MainRoutes.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const MainRoutes = () => {
-  return <div>MainRoutes</div>;
+  return <div></div>;
 };
 
 export default MainRoutes;
