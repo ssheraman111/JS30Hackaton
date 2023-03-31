c7a964d8f36892f72aa1b5d69903258c2e2b043d
Bekzat <Bekzat@Bekzat-MacBook-Pro.local>
2023-03-31 12:52:28 +0600

    AuthStart

--- src/routes/MainRoutes.js
+++ src/routes/MainRoutes.js
@@ -4,13 +4,16 @@ import { Route, Routes } from "react-router-dom";
 import AdminPage from "../components/Pages/AdminPage/AdminPage";
 import EditPage from "../components/Pages/EditPage/EditPage";
 import HomePage from "../components/Pages/HomePage/HomePage";
+import NotFoundPage from "../components/NotFoundPage/NotFoundPage";
+import Auth from "../components/Auth/Auth";
 
 const MainRoutes = () => {
   const PUBLIC_ROUTES = [
     { link: "/", element: <HomePage />, id: 1 },
     { link: "/admin", element: <AdminPage />, id: 2 },
     { link: "/edit/:id", element: <EditPage />, id: 3 },
-    { link: "/edit/:id", element: <EditPage />, id: 3 },
+    { link: "/auth", element: <Auth />, id: 4 },
+    { link: "*", element: <NotFoundPage />, id: 5 },
   ];
   return (
     <Routes>
