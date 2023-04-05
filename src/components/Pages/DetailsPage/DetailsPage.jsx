aa4d47d40a33069a273fdbe7836c415f8aff2c65
Bekzat <Bekzat@Bekzat-MacBook-Pro.local>
2023-04-05 17:03:51 +0600

    adptiv

--- src/components/Pages/DetailsPage/DetailsPage.jsx
+++ src/components/Pages/DetailsPage/DetailsPage.jsx
@@ -3,6 +3,7 @@ import { Box, Typography } from "@mui/material";
 import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useProducts } from "../../../Context/ProductContextProvider";
+import "./DetailsPage.css";
 
 const DetailsPage = () => {
   const {
@@ -20,17 +21,21 @@ const DetailsPage = () => {
   }, []);
 
   return (
-    <Box>
+    <Box className="details__container">
       <Box className="left_block_of_img">
-        <img src={product.picture} alt="" />
+        <img className="details__img" src={product.picture} alt="" />
       </Box>
-      <Box>
-        <Typography variant="h5">{product.name}</Typography>
-        <Typography>{product.price}</Typography>
-        <hr />
-        <Typography>
+      <Box className="rigth_block_of_details">
+        <Typography className="details__name" variant="h5">
+          {product.name}
+        </Typography>
+        <Typography className="details__price">${product.price}</Typography>
+
+        <Typography className="titleOfDescr">
           Short Description:
-          <Typography>{product.description}</Typography>
+          <Typography className="details__descr">
+            {product.description}
+          </Typography>
         </Typography>
       </Box>
     </Box>
